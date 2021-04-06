feb5744bde5e23be532aaadd74212cf023a50096
jj-krehbiel <72628524+jj-krehbiel@users.noreply.github.com>
2021-04-06 18:23:59 -0400

    pull values from json

--- static/app.js
+++ static/app.js
@@ -1,4 +1,14 @@
 // Fetch the JSON data and console log it
 d3.json("../interactive-visualization-challenge/samples.json").then(function(data) {
     console.log(data);
+     // Grab values from the response json object to build the plots
+     var human = data.samples.map(sample => sample.id);
+     var values = data.samples.map(sample => sample.sample_values.slice(0,10));
+     var ids = data.samples.map(sample => sample.otu_ids.slice(0,10));
+     var otuLabels = data.samples.map(sample => sample.otu_labels.slice(0,10));
+     console.log(human);
+     console.log(values);
+     console.log(ids);
+     console.log(otuLabels);
   });
+
