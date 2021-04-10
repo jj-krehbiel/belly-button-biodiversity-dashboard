c1bfbefb325a48bb66d97f8163a3b91ad438d41c
jj-krehbiel <72628524+jj-krehbiel@users.noreply.github.com>
2021-04-10 17:42:39 -0400

    fix bar graph, add change function

--- static/app.js
+++ static/app.js
@@ -1,39 +1,72 @@
+// Build initial plot
+function init(){
+    buildPlot()
+}
+
+// Create option change function
+function optionChanged() {
+    buildPlot();
+}
+
 // Fetch the JSON data and console log it
-const path = "../interactive-visualization-challenge/samples.json"
-d3.json(path).then(function(data) {
-    console.log(data);
-     // Grab values from the response json object to build the plots
-    var human = data.names;
-    var human_id = human[0];
-    console.log(human_id);
-    var sample = data.samples.filter(sample => sample.id === human_id)[0];
-    console.log(sample);
-    var values = sample.sample_values.slice(0,10).reverse();
-    var otu_ids = sample.otu_ids.slice(0,10).map(id => "otu " + id).reverse();
-    var otu_id_no = data.samples.map(sample => sample.otu_ids.slice(0,10));
-    var bubblex = +otu_id_no;
-    var otuLabels = data.samples.map(sample => sample.otu_labels.slice(0,10));
-    var ethnicity = data.metadata.map(metadata => metadata.ethnicity);
-    var gender = data.metadata.map(metadata => metadata.gender);
-    var age = data.metadata.map(metadata => metadata.age);
-    var location = data.metadata.map(metadata => metadata.location);
-    var bbtype = data.metadata.map(metadata => metadata.bbtype);
-    var wfreq = data.metadata.map(metadata => metadata.wfreq);
-    
-    // Make dropdown menu using human array
-    var dropdown = d3.select("select");
-    human.forEach(human => {
-        dropdown.append("option").text(human).property("value", human);
-    });
+function buildPlot(){
+    const path = "../interactive-visualization-challenge/samples.json"
+    d3.json(path).then(function(data) {
+        console.log(data);
+        // Grab values from the response json object to build the plots
+        var human = data.names;
+        // var human_id = human[0];
+        // console.log(human_id);
+        // var sample = data.samples.filter(sample => sample.id === human_id)[0];
+        // console.log(sample);
+        // var values = sample.sample_values.slice(0,10).reverse();
+        // var otu_ids = sample.otu_ids.slice(0,10).map(id => "otu " + id).reverse();
+        // var otu_id_no = data.samples.map(sample => sample.otu_ids.slice(0,10));
+        // var bubblex = +otu_id_no;
+        // var otuLabels = data.samples.map(sample => sample.otu_labels.slice(0,10));
+        // var ethnicity = data.metadata.map(metadata => metadata.ethnicity);
+        // var gender = data.metadata.map(metadata => metadata.gender);
+        // var age = data.metadata.map(metadata => metadata.age);
+        // var location = data.metadata.map(metadata => metadata.location);
+        // var bbtype = data.metadata.map(metadata => metadata.bbtype);
+        // var wfreq = data.metadata.map(metadata => metadata.wfreq);
+        
+        // Make dropdown menu using human array
+        var dropdown = d3.select("select");
+        human.forEach(human => {
+            dropdown.append("option").text(human).property("value", human);
+        });
 
-    // Assign a variable to the value in the dropdown menu
-    var selectedHuman = d3.selectAll("#selDataset").node().value;
-    console.log(selectedHuman);
+        // Assign a variable to the value in the dropdown menu
+        var selectedHuman = d3.selectAll("#selDataset").node().value;
+        console.log(selectedHuman);
 
-    // filter data based on selectedHuman
-    filteredData = data.samples.filter(option => option.id == selectedHuman)
+        // filter data based on selectedHuman
+        filteredData = data.samples.filter(option => option.id == selectedHuman)
+        console.log(filteredData);
 
-    console.log(filteredData);
+        var traceBar = {
+            x: filteredData[0].sample_values.slice(0,10).reverse(),
+            y: filteredData[0].otu_ids.slice(0, 10).reverse().map(int => "OTU " + int.toString()),
+            text: filteredData[0].otu_labels.slice(0,10).reverse(),
+            type:"bar",
+            orientation: 'h'
+        };
+
+        var dataBar = [traceBar];
+
+        var layout = {
+            title : 'Top 10 OTU samples',
+            margin: {
+                l: 75,
+                r: 100,
+                t: 60,
+                b: 60
+            }
+        };
+
+        Plotly.newPlot("bar", dataBar, layout);
+
 
     // console.log(human);
     // console.log(values);
@@ -47,41 +80,41 @@ d3.json(path).then(function(data) {
     // console.log(wfreq);
 
     // Make a function to display default bar chart
-    function init() {
+    // function init() {
      
-        var trace1 = {
-            x: values,
-            y: otu_ids,
-            text: otuLabels,
-            orientation: "h",
-            type: "bar"
-        };
+    //     var trace1 = {
+    //         x: values,
+    //         y: otu_ids,
+    //         text: otuLabels,
+    //         orientation: "h",
+    //         type: "bar"
+    //     };
         
-        var data = [trace1];
+    //     var data = [trace1];
         
-        Plotly.newPlot("bar", data);
+    //     Plotly.newPlot("bar", data);
 
         // Display default bubble chart
-        var trace2 = {
-            x: parseInt(otu_ids),
-            y: values,
-            // mode: 'marker',
-            text: otuLabels,
-            marker:{
-                size: values,
-                color: otu_ids
-            }
-        };
+        // var trace2 = {
+        //     x: parseInt(otu_ids),
+        //     y: values,
+        //     // mode: 'marker',
+        //     text: otuLabels,
+        //     marker:{
+        //         size: values,
+        //         color: otu_ids
+        //     }
+        // };
 
-        var data2 = [trace2];
+        // var data2 = [trace2];
 
-        var layout = {
-            showlegend: false,
-            // height: 600,
-            // width: 1800
-        };
+        // var layout = {
+        //     showlegend: false,
+        //     // height: 600,
+        //     // width: 1800
+        // };
 
-        Plotly.newPlot("bubble", data2, layout);
+        // Plotly.newPlot("bubble", data2, layout);
         // console.log("bubblex: ", bubblex)
 
     //     // Display default demographic data
@@ -105,10 +138,7 @@ d3.json(path).then(function(data) {
 
     // };
 
-    init();
-
-    };
-
-
 });
+};
 
+init()
